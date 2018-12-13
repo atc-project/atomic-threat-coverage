cef732d331f866c69b44c96b6a3b88d42bb7a7a7
Yugoslavskiy Daniil <yugoslavskiy@gmail.com>
2018-12-13 18:56:23 +0100

    init_markdown.sh updated

--- scripts/init_markdown.sh
+++ scripts/init_markdown.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-mkdir ../Atomic_Threat_Coverage
-mkdir ../Atomic_Threat_Coverage/Detection_Rules
-mkdir ../Atomic_Threat_Coverage/Logging_Policies
-mkdir ../Atomic_Threat_Coverage/Data_Needed
-mkdir ../Atomic_Threat_Coverage/Triggering
+DIRECTORIES=(
+  "Atomic_Threat_Coverage"
+  "Atomic_Threat_Coverage/Detection_Rules"
+  "Atomic_Threat_Coverage/Logging_Policies"
+  "Atomic_Threat_Coverage/Data_Needed"
+  "Atomic_Threat_Coverage/Triggering"
+)
+
+for DIRECTORY in ${DIRECTORIES[@]}; do
+  if [[ ! -d ${DIRECTORY} ]]; then
+    mkdir ${DIRECTORY}
+  fi
+done
