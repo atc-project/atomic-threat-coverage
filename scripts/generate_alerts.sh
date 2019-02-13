e3efa850a900f9e118fa557bea01361c82ad3c49
Yugoslavskiy Daniil <yugoslavskiy@gmail.com>
2019-02-13 03:15:48 +0100

    deleted old scripts directory

--- scripts/generate_alerts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for FILE in $( find sigma/rules -name "*.yml" ); do
-  FILENAME=$( basename ${FILE} | cut -f 1 -d '.' )
-  python3 sigma/tools/sigmac -t es-qs --ignore-backend-errors -o rules-md/${FILENAME}-es-qs.md ${FILE}
-  python3 sigma/tools/sigmac -t graylog --ignore-backend-errors -o rules-md/${FILENAME}-graylog.md ${FILE}
-done
