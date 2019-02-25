1c95abed9ce019c1706bb2d135c0267806178f90
Thomas Bouve <crahan@n00.be>
2019-02-25 22:10:32 +0100

    Fix for hardcoded init_markdown.sh folder paths.

--- scripts/populatemarkdown.py
+++ scripts/populatemarkdown.py
@@ -35,7 +35,7 @@ class PopulateMarkdown:
             self.atc_dir = atc_dir
 
         else:
-            self.atc_dir = '../'+ATCconfig.get('md_name_of_root_directory')+'/'
+            self.atc_dir = ATCconfig.get('md_name_of_root_directory')+'/'
 
         # Check if art_dir provided
         if art_dir:
