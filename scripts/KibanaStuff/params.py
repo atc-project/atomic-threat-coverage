cbb179f7d9e1e0b5d0ff07317081fbed006b7481
mrblacyk <kweinzettl@gmail.com>
2019-03-09 04:38:33 -0800

    Preparation for series params handling

---
+++ scripts/KibanaStuff/params.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+from metrics import BaseKibanaSeriesParams
+
+# ########################################################################### #
+# ############################ Params ####################################### #
+# ########################################################################### #
+
+
+class AverageAgg(BaseKibanaSeriesParams):
+
+    def __init__(self):
+        pass
+
+
+class CountAgg(BaseKibanaSeriesParams):
+
+    def __init__(self):
+        pass
+
+
+class MaxAgg(BaseKibanaSeriesParams):
+
+    def __init__(self):
+        pass
+
+
+class MedianAgg(BaseKibanaSeriesParams):
+
+    def __init__(self):
+        pass
+
+
+class MinAgg(BaseKibanaSeriesParams):
+
+    def __init__(self):
+        pass
+
+
+class PercentileRanksAgg(BaseKibanaSeriesParams):
+
+    def __init__(self):
+        pass
+
+
+class PercentilesAgg(BaseKibanaSeriesParams):
+
+    def __init__(self):
+        pass
+
+
+class StandardDeviationAgg(BaseKibanaSeriesParams):
+
+    def __init__(self):
+        pass
+
+
+class SumAgg(BaseKibanaSeriesParams):
+
+    def __init__(self):
+        pass
+
+
+class TopHitsAgg(BaseKibanaSeriesParams):
+
+    def __init__(self):
+        pass
+
+
+class UniqueCountAgg(BaseKibanaSeriesParams):
+
+    def __init__(self):
+        pass
+
+
+class DotSizeAgg(BaseKibanaSeriesParams):
+
+    def __init__(self):
+        pass
