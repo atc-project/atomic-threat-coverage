1d53fcce5071d1b9d8748caf13a058579b2430ce
mrblacyk <kweinzettl@gmail.com>
2019-03-09 04:13:20 -0800

    Metric Aggregations

--- scripts/KibanaStuff/aggs.py
+++ scripts/KibanaStuff/aggs.py
@@ -87,28 +87,77 @@ class PercentilesAgg(BaseKibanaAgg):
 
 class StandardDeviationAgg(BaseKibanaAgg):
 
-    def __init__(self, id, enabled=None):
+    def __init__(self, id, field, enabled=None):
 
-        super().__init__()
+        super().__init__(
+            id=id, enabled=enabled, params={
+                "field": field
+            }, schema="metric", type="std_dev"
+        )
 
 
 class SumAgg(BaseKibanaAgg):
 
-    def __init__(self, id, enabled=None):
+    def __init__(self, id, field, enabled=None):
 
-        super().__init__()
+        super().__init__(
+            id=id, enabled=enabled, params={
+                "field": field
+            }, schema="metric", type="sum"
+        )
 
 
 class TopHitsAgg(BaseKibanaAgg):
 
-    def __init__(self, id, enabled=None):
-
-        super().__init__()
+    def __init__(self, id, field, aggregate_with, size, sort_order, sort_field,
+                 enabled=None):
+        """aggregate_with - can be average, max, min or sum
+size - integer
+sort_order - can be asc or dsc
+"""
+        super().__init__(
+            id=id, enabled=enabled, params={
+                "aggregate": aggregate_with, "field": field,
+                "size": size, "sortField": sort_field, "sortOrder": sort_order
+            }, schema="metric", type="top_hits"
+        )
 
 
 class UniqueCountAgg(BaseKibanaAgg):
 
-    def __init__(self, id, enabled=None):
+    def __init__(self, id, field, enabled=None):
 
-        super().__init__()
+        super().__init__(
+            id=id, enabled=enabled, params={
+                "field": field
+            }, schema="metric", type="cardinality"
+        )
 
+
+class DotSizeAgg(BaseKibanaAgg):
+
+    def __init__(
+        self, id, aggregation_type, field=None, aggregate_with=None, size=None,
+        enabled=None, order=None
+    ):
+
+        if aggregation_type in ["avg", "max", "min", "sum", "cardinality"]\
+                and not field:
+            raise Exception("Field 'field' required for given aggregation " +
+                            "type")
+
+        if aggregation_type == "top_hits" and not field and not aggregate_with\
+                and not size and not order:
+            raise Exception("""For Top Hits following fields are also required:
+* field - valid field for given index/search
+* aggregate_with - avg/min/max/sum
+* size - integer
+* order - asc/dsc
+""")
+
+        super().__init__(
+            id=id, enabled=enabled, type=aggregation_type, schema="radius",
+            params={
+                "field": field
+            }
+        )
